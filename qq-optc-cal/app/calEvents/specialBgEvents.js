8ce9801f058f006696c5c12a843ca0e031497ce9
Zifeng Lu <lukforce@gmail.com>
2017-03-21 00:35:38 -0700

    New sugo

--- qq-optc-cal/app/calEvents/specialBgEvents.js
+++ qq-optc-cal/app/calEvents/specialBgEvents.js
@@ -5,6 +5,11 @@ var specialBgEvents = [
         end: '2017-03-29'
     },
     {
+        id: 'sugo',
+        start: '2017-03-23',
+        end: '2017-03-25'
+    },
+    {
         id: 'meat_up',
         start: '2017-03-24',
         end: '2017-04-01'
