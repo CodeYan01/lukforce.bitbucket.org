86e038a8fa6b34a30f1000c7834e986bb0777add
Zifeng Lu <lukforce@gmail.com>
2017-03-23 19:54:27 -0700

    Fortnight and Raid update

--- qq-optc-cal/app/calEvents/fortnightEvents.js
+++ qq-optc-cal/app/calEvents/fortnightEvents.js
@@ -3,60 +3,168 @@ var fortnightEvents = [
         id: '1049',
         start: '2017-02-28',
         end: '2017-03-14',
-        is_replay: false
+        is_replay: false,
+        has_ranking: false
     },
     {
         id: '392',
         start: '2017-02-28',
         end: '2017-03-07',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
     },
     {
         id: '462',
         start: '2017-02-28',
         end: '2017-03-07',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
     },
     {
         id: '232',
         start: '2017-03-07',
         end: '2017-03-14',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
     },
     {
         id: '656',
         start: '2017-03-07',
         end: '2017-03-14',
-        is_replay: true
+        is_replay: true,
+        has_ranking: true
     },
     {
         id: '1137',
         start: '2017-03-14',
         end: '2017-03-28',
-        is_replay: false
+        is_replay: false,
+        has_ranking: false
     },
     {
         id: '263',
         start: '2017-03-14',
         end: '2017-03-21',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
     },
     {
         id: '929',
         start: '2017-03-14',
         end: '2017-03-21',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
     },
     {
         id: '268',
         start: '2017-03-21',
         end: '2017-03-28',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
     },
     {
         id: '852',
         start: '2017-03-21',
         end: '2017-03-28',
-        is_replay: true
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '297',
+        start: '2017-03-28',
+        end: '2017-04-04',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '724',
+        start: '2017-03-28',
+        end: '2017-04-04',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '341',
+        start: '2017-04-04',
+        end: '2017-04-11',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '599',
+        start: '2017-04-04',
+        end: '2017-04-11',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '797',
+        start: '2017-04-04',
+        end: '2017-04-11',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '918',
+        start: '2017-04-04',
+        end: '2017-04-11',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '318',
+        start: '2017-04-11',
+        end: '2017-04-18',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '436',
+        start: '2017-04-11',
+        end: '2017-04-18',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '445',
+        start: '2017-04-11',
+        end: '2017-04-18',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '538',
+        start: '2017-04-11',
+        end: '2017-04-18',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '30',
+        start: '2017-04-18',
+        end: '2017-04-25',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '510',
+        start: '2017-04-18',
+        end: '2017-04-25',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '337',
+        start: '2017-04-25',
+        end: '2017-05-02',
+        is_replay: true,
+        has_ranking: false
+    },
+    {
+        id: '946',
+        start: '2017-04-25',
+        end: '2017-05-02',
+        is_replay: true,
+        has_ranking: false
     }
 ];
