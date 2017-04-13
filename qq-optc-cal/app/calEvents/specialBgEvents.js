a58a03128901abb02c49cab5017d914fec8059d3
Zifeng Lu <lukforce@gmail.com>
2017-04-13 00:52:10 -0700

    Clash of Clash, sp up

--- qq-optc-cal/app/calEvents/specialBgEvents.js
+++ qq-optc-cal/app/calEvents/specialBgEvents.js
@@ -18,5 +18,15 @@ var specialBgEvents = [
         id: 'sugo',
         start: '2017-04-07',
         end: '2017-04-09'
+    },
+    {
+        id: 'sp_up',
+        start: '2017-04-18',
+        end: '2017-04-20'
+    },
+    {
+        id: 'sp_up',
+        start: '2017-04-25',
+        end: '2017-04-27'
     }
 ]
