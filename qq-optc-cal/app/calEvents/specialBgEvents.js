c271759ed6372ebf8622a24c9ee990dc1fe403d6
Zifeng Lu <lukforce@gmail.com>
2017-04-27 11:25:46 -0700

    Biweekly event update

--- qq-optc-cal/app/calEvents/specialBgEvents.js
+++ qq-optc-cal/app/calEvents/specialBgEvents.js
@@ -33,5 +33,20 @@ var specialBgEvents = [
         id: 'sp_up',
         start: '2017-04-25',
         end: '2017-04-27'
+    },
+    {
+        id: 'meat_up',
+        start: '2017-05-02',
+        end: '2017-05-15'
+    },
+    {
+        id: 'sp_up',
+        start: '2017-05-02',
+        end: '2017-05-04'
+    },
+    {
+        id: 'sp_up',
+        start: '2017-05-09',
+        end: '2017-05-11'
     }
 ]
