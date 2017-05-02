f122702431689904388192c4691ce323e22d4d7a
Zifeng Lu <lukforce@gmail.com>
2017-05-02 16:24:39 -0700

    Ambush support

--- qq-optc-cal/app/calEvents/raidEvents.js
+++ qq-optc-cal/app/calEvents/raidEvents.js
@@ -1,102 +1,127 @@
 var raidEvents = [
     {
         id: '603',
-        start: '2017-03-02'
+        start: '2017-03-02',
+        ambush: null
     },
     {
         id: '447',
-        start: '2017-03-05'
+        start: '2017-03-05',
+        ambush: null
     },
     {
         id: '1047',
-        start: '2017-03-09'
+        start: '2017-03-09',
+        ambush: null
     },
     {
         id: '516',
-        start: '2017-03-12'
+        start: '2017-03-12',
+        ambush: null
     },
     {
         id: '575',
-        start: '2017-03-16'
+        start: '2017-03-16',
+        ambush: null
     },
     {
         id: '978',
-        start: '2017-03-19'
+        start: '2017-03-19',
+        ambush: null
     },
     {
         id: '836',
-        start: '2017-03-23'
+        start: '2017-03-23',
+        ambush: 'YWB'
     },
     {
         id: '306',
-        start: '2017-03-24'
+        start: '2017-03-24',
+        ambush: 'YWB'
     },
     {
         id: '771',
-        start: '2017-03-25'
+        start: '2017-03-25',
+        ambush: 'YWB'
     },
     {
         id: '418',
-        start: '2017-03-26'
+        start: '2017-03-26',
+        ambush: 'YWB'
     },
     {
         id: '227',
-        start: '2017-03-30'
+        start: '2017-03-30',
+        ambush: null
     },
     {
         id: '989',
-        start: '2017-04-01'
+        start: '2017-04-01',
+        ambush: null
     },
     {
         id: '390',
-        start: '2017-04-02'
+        start: '2017-04-02',
+        ambush: null
     },
     {
         id: '865',
-        start: '2017-04-06'
+        start: '2017-04-06',
+        ambush: null
     },
     {
         id: '518',
-        start: '2017-04-09'
+        start: '2017-04-09',
+        ambush: null
     },
     {
         id: '249',
-        start: '2017-04-13'
+        start: '2017-04-13',
+        ambush: null
     },
     {
         id: '978',
-        start: '2017-04-16'
+        start: '2017-04-16',
+        ambush: null
     },
     {
         id: '1298',
-        start: '2017-04-20'
+        start: '2017-04-20',
+        ambush: 'YWB'
     },
     {
         id: '865',
-        start: '2017-04-21'
+        start: '2017-04-21',
+        ambush: 'YWB'
     },
     {
         id: '447',
-        start: '2017-04-22'
+        start: '2017-04-22',
+        ambush: 'YWB'
     },
     {
         id: '418',
-        start: '2017-04-23'
+        start: '2017-04-23',
+        ambush: 'YWB'
     },
     {
         id: '654',
-        start: '2017-04-27'
+        start: '2017-04-27',
+        ambush: null
     },
     {
         id: '227',
-        start: '2017-04-30'
+        start: '2017-04-30',
+        ambush: null
     },
     {
         id: '836',
-        start: '2017-05-04'
+        start: '2017-05-04',
+        ambush: 'Shanks'
     },
     {
         id: '603',
-        start: '2017-05-07'
+        start: '2017-05-07',
+        ambush: 'Shanks'
     }
 ]
