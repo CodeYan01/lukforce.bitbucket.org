239538e14a7d8fb33e074efa7b936816ce292ada
Zifeng Lu <lukforce@gmail.com>
2017-07-12 16:41:20 -0700

    Coliseums event revamp

---
+++ optc-cal/app/calEvents/coliseumEvents.js
@@ -0,0 +1,54 @@
+var ywb = '1258';
+var shanks = '1380';
+
+var coliseumEvents = [
+    {
+        newId: [983, 984],
+        repId: [781, 862, 834],
+        start: '2017-02-28',
+        end: '2017-03-19',
+        ambush: null
+    },
+    {
+        newId: [1016, 1019],
+        repId: [882, 779],
+        start: '2017-03-21',
+        end: '2017-04-09',
+        ambush: null
+    },
+    {
+        newId: [1041, 1018],
+        repId: [806, 926],
+        start: '2017-04-11',
+        end: '2017-04-30',
+        ambush: null
+    },
+    {
+        newId: [1043, 1091],
+        repId: [777, 833, 860],
+        start: '2017-05-02',
+        end: '2017-05-21',
+        ambush: null
+    },
+    {
+        newId: [],
+        repId: [831, 882, 951, 983, 862],
+        start: '2017-05-23',
+        end: '2017-06-11',
+        ambush: null
+    },
+    {
+        newId: [1102, 1104, 1106],
+        repId: [884, 903],
+        start: '2017-06-13',
+        end: '2017-07-02',
+        ambush: null
+    },
+    {
+        newId: [1087, 1089],
+        repId: [781, 924, 834],
+        start: '2017-07-04',
+        end: '2017-07-23',
+        ambush: null
+    },
+]
