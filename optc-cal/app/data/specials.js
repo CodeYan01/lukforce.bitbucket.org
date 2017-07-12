a8b4a05987b364a2eb248775e850ffabc37dffe3
Zifeng Lu <lukforce@gmail.com>
2017-07-12 14:54:43 -0700

    Added special events: Secret Stage

---
+++ optc-cal/app/data/specials.js
@@ -0,0 +1,27 @@
+var specials = {
+    'ss_0394': {
+        type: 'Secret Stage',
+        thumb: '0394',
+        name: 'Dog Penguin'
+    },
+    'ss_0436': {
+        type: 'Secret Stage',
+        thumb: '0436',
+        name: 'Sanji Candy'
+    },
+    'ss_0511': {
+        type: 'Secret Stage',
+        thumb: '0511',
+        name: 'Mr. 1'
+    },
+    'ss_0584': {
+        type: 'Secret Stage',
+        thumb: '0584',
+        name: 'Aisa'
+    },
+    'ss_0716': {
+        type: 'Secret Stage',
+        thumb: '0716',
+        name: 'Conis'
+    },
+}
