34bb004c64ef392866402b29171c5c075bdce9d6
Zifeng Lu <lukforce@gmail.com>
2017-07-12 23:13:23 -0700

    Code clean up

--- optc-cal/app/data/fortnights.js
+++ optc-cal/app/data/fortnights.js
@@ -1,386 +1,258 @@
 var fortnights = {
     30: {
         name: 'Alvida',
-        thumb: '0030',
-        drop: '',
-        gw: ''
+        thumb: '0030'
     },
     39: {
         name: 'Baggy',
-        thumb: '0039',
-        drop: '',
-        gw: ''
+        thumb: '0039'
     },
     46: {
         name: 'Kuro',
-        thumb: '0046',
-        drop: '',
-        gw: ''
+        thumb: '0046'
     },
     58: {
         name: 'Krieg',
-        thumb: '0058',
-        drop: '',
-        gw: ''
+        thumb: '0058'
     },
     66: {
         name: 'Arlong',
-        thumb: '0066',
-        drop: '',
-        gw: ''
+        thumb: '0066'
     },
     228: {
         name: 'Morgan',
-        thumb: '0228',
-        drop: '',
-        gw: ''
+        thumb: '0228'
     },
     232: {
         name: 'Hina',
-        thumb: '0232',
-        drop: '',
-        gw: ''
+        thumb: '0232'
     },
     263: {
         name: 'CobiMeppo',
-        thumb: '0263',
-        drop: '',
-        gw: ''
+        thumb: '0263'
     },
     268: {
         name: 'Smoker',
-        thumb: '0268',
-        drop: '',
-        gw: ''
+        thumb: '0268'
     },
     297: {
         name: 'Tsuru',
-        thumb: '0297',
-        drop: '',
-        gw: ''
+        thumb: '0297'
     },
     318: {
         name: 'Kalifa',
-        thumb: '0318',
-        drop: '',
-        gw: ''
+        thumb: '0318'
     },
     322: {
         name: 'Lucci',
-        thumb: '0322',
-        drop: '',
-        gw: ''
+        thumb: '0322'
     },
     337: {
         name: 'Franky',
-        thumb: '0337',
-        drop: '',
-        gw: ''
+        thumb: '0337'
     },
     341: {
         name: 'Kraken',
-        thumb: '0341',
-        drop: '',
-        gw: ''
+        thumb: '0341'
     },
     386: {
         name: 'Hogback',
-        thumb: '0386',
-        drop: '',
-        gw: ''
+        thumb: '0386'
     },
     392: {
         name: 'Perona',
-        thumb: '0392',
-        drop: '',
-        gw: ''
+        thumb: '0392'
     },
     405: {
         name: 'Absalom',
-        thumb: '0405',
-        drop: '',
-        gw: ''
+        thumb: '0405'
     },
     420: {
         name: 'Kimono Luffy',
-        thumb: '0420',
-        drop: '',
-        gw: ''
+        thumb: '0420'
     },
     424: {
         name: 'Brook',
-        thumb: '0424',
-        drop: '',
-        gw: ''
+        thumb: '0424'
     },
     436: {
         name: 'Kamabakka',
-        thumb: '0436',
-        drop: '',
-        gw: ''
+        thumb: '0436'
     },
     445: {
         name: 'Karoo',
-        thumb: '0445',
-        drop: '',
-        gw: ''
+        thumb: '0445'
     },
     462: {
         name: 'Prisoner BW',
-        thumb: '0462',
-        drop: '',
-        gw: ''
+        thumb: '0462'
     },
     510: {
         name: 'Prisoner Croc',
-        thumb: '0510',
-        drop: '',
-        gw: ''
+        thumb: '0510'
     },
     538: {
         name: 'Domino',
-        thumb: '0538',
-        drop: '',
-        gw: ''
+        thumb: '0538'
     },
     550: {
         name: 'Sadie',
-        thumb: '0550',
-        drop: '',
-        gw: ''
+        thumb: '0550'
     },
     571: {
         name: 'Afro Luffy',
-        thumb: '0571',
-        drop: '',
-        gw: ''
+        thumb: '0571'
     },
     583: {
         name: 'Shandia',
-        thumb: '0583',
-        drop: '',
-        gw: ''
+        thumb: '0583'
     },
     599: {
         name: 'Pirate Apprentices',
-        thumb: '0599',
-        drop: '',
-        gw: ''
+        thumb: '0599'
     },
     607: {
         name: 'Zeff',
-        thumb: '0607',
-        drop: '',
-        gw: ''
+        thumb: '0607'
     },
     635: {
         name: 'Camie',
-        thumb: '0635',
-        drop: '',
-        gw: ''
+        thumb: '0635'
     },
     656: {
         name: 'Arlong Pirates',
-        thumb: '0656',
-        drop: '',
-        gw: ''
+        thumb: '0656'
     },
     664: {
         name: 'Sand Sand Band',
-        thumb: '0664',
-        drop: '',
-        gw: ''
+        thumb: '0664'
     },
     690: {
         name: 'Calgara & Noland',
-        thumb: '0690',
-        drop: '',
-        gw: ''
+        thumb: '0690'
     },
     713: {
         name: 'Ganfor',
-        thumb: '0713',
-        drop: '',
-        gw: ''
+        thumb: '0713'
     },
     724: {
         name: 'T-Bone',
-        thumb: '0724',
-        drop: '',
-        gw: ''
+        thumb: '0724'
     },
     743: {
         name: 'Wanze',
-        thumb: '0743',
-        drop: '',
-        gw: ''
+        thumb: '0743'
     },
     761: {
         name: 'Halloween 1',
-        thumb: '0761',
-        drop: '',
-        gw: ''
+        thumb: '0761'
     },
     766: {
         name: 'Halloween 2',
-        thumb: '0766',
-        drop: '',
-        gw: ''
+        thumb: '0766'
     },
     797: {
         name: 'Breed',
-        thumb: '0797',
-        drop: '',
-        gw: ''
+        thumb: '0797'
     },
     808: {
         name: 'Wedding Nami',
-        thumb: '0808',
-        drop: '',
-        gw: ''
+        thumb: '0808'
     },
     848: {
         name: 'Tom\'s Workers',
-        thumb: '0848',
-        drop: '',
-        gw: ''
+        thumb: '0848'
     },
     852: {
         name: 'Wapol',
-        thumb: '0852',
-        drop: '',
-        gw: ''
+        thumb: '0852'
     },
     918: {
         name: 'Kuja',
-        thumb: '0918',
-        drop: '',
-        gw: ''
+        thumb: '0918'
     },
     929: {
         name: 'Hannyabal',
-        thumb: '0929',
-        drop: '',
-        gw: ''
+        thumb: '0929'
     },
     946: {
         name: 'WB Pirates 1',
-        thumb: '0946',
-        drop: '',
-        gw: ''
+        thumb: '0946'
     },
     979: {
         name: 'WB Pirates 2',
-        thumb: '0979',
-        drop: '',
-        gw: ''
+        thumb: '0979'
     },
     1003: {
         name: 'Heart Pirates',
-        thumb: '1003',
-        drop: '',
-        gw: ''
+        thumb: '1003'
     },
     1011: {
         name: 'Count Butler',
-        thumb: '1011',
-        drop: '',
-        gw: ''
+        thumb: '1011'
     },
     1037: {
         name: 'Ivankov',
-        thumb: '1037',
-        drop: '',
-        gw: ''
+        thumb: '1037'
     },
     1049: {
         name: 'Nico Olvia',
-        thumb: '1049',
-        drop: '',
-        gw: ''
+        thumb: '1049'
     },
     1072: {
         name: 'Dead End',
-        thumb: '1072',
-        drop: '',
-        gw: ''
+        thumb: '1072'
     },
     1095: {
         name: 'Gold Lion Pirates',
-        thumb: '1095',
-        drop: '',
-        gw: ''
+        thumb: '1095'
     },
     1112: {
         name: 'Young Nami Robin',
-        thumb: '1112',
-        drop: '',
-        gw: ''
+        thumb: '1112'
     },
     1137: {
         name: 'CP9',
-        thumb: '1137',
-        drop: '',
-        gw: ''
+        thumb: '1137'
     },
     1167: {
         name: 'Gran Tesoro',
-        thumb: '1167',
-        drop: '',
-        gw: ''
+        thumb: '1167'
     },
     1203: {
         name: 'Rumbar Pirates',
-        thumb: '1203',
-        drop: '',
-        gw: ''
+        thumb: '1203'
     },
     1215: {
         name: 'World Pirates',
-        thumb: '1215',
-        drop: '',
-        gw: ''
+        thumb: '1215'
     },
     1242: {
         name: 'Revolution Army',
-        thumb: '1242',
-        drop: '',
-        gw: ''
+        thumb: '1242'
     },
     1254: {
         name: 'WB Pirates 3',
-        thumb: '1254',
-        drop: '',
-        gw: ''
+        thumb: '1254'
     },
     1287: {
         name: 'Donquixote Kids',
-        thumb: '1287',
-        drop: '',
-        gw: ''
+        thumb: '1287'
     },
     1302: {
         name: 'Prisoner Baggy',
-        thumb: '1302',
-        drop: '',
-        gw: ''
+        thumb: '1302'
     },
     1328: {
         name: 'Tontatta 1',
-        thumb: '1328',
-        drop: '',
-        gw: ''
+        thumb: '1328'
     },
     1332: {
         name: 'Tontatta 2',
-        thumb: '1332',
-        drop: '',
-        gw: ''
+        thumb: '1332'
     },
     1357: {
         name: 'Big Mom Pirates',
-        thumb: '1357',
-        drop: '',
-        gw: ''
+        thumb: '1357'
     },
 };
--- optc-cal/app/data/raids.js
+++ optc-cal/app/data/raids.js
@@ -1,122 +1,98 @@
 var raids = {
     227: {
         name: 'Mihawk',
-        thumb: '0227',
-        gw: ''
+        thumb: '0227'
     },
     249: {
         name: 'M. Chopper',
-        thumb: '0249',
-        gw: ''
+        thumb: '0249'
     },
     306: {
         name: 'Garp',
-        thumb: '0306',
-        gw: ''
+        thumb: '0306'
     },
     353: {
         name: 'Zephyr',
-        thumb: '0353',
-        gw: ''
+        thumb: '0353'
     },
     390: {
         name: 'Ivankov',
-        thumb: '0390',
-        gw: ''
+        thumb: '0390'
     },
     418: {
         name: 'Doflamingo',
-        thumb: '0418',
-        gw: ''
+        thumb: '0418'
     },
     447: {
         name: 'Blackbeard',
-        thumb: '0447',
-        gw: ''
+        thumb: '0447'
     },
     516: {
         name: 'Heracles',
-        thumb: '0516',
-        gw: ''
+        thumb: '0516'
     },
     518: {
         name: 'Usopp-un',
-        thumb: '0518',
-        gw: ''
+        thumb: '0518'
     },
     575: {
         name: 'Aokiji',
-        thumb: '00575',
-        gw: ''
+        thumb: '00575'
     },
     603: {
         name: 'Eneru',
-        thumb: '0603',
-        gw: ''
+        thumb: '0603'
     },
     654: {
         name: 'Duval',
-        thumb: '0654',
-        gw: ''
+        thumb: '0654'
     },
     771: {
         name: 'BusterCall',
-        thumb: '0771',
-        gw: ''
+        thumb: '0771'
     },
     795: {
         name: 'NM Luffy',
-        thumb: '0795',
-        gw: ''
+        thumb: '0795'
     },
     836: {
         name: 'Kuma',
-        thumb: '0836',
-        gw: ''
+        thumb: '0836'
     },
     865: {
         name: 'Boa',
-        thumb: '0865',
-        gw: ''
+        thumb: '0865'
     },
     933: {
         name: 'Pica',
-        thumb: '0933',
-        gw: ''
+        thumb: '0933'
     },
     978: {
         name: 'Kizaru',
-        thumb: '0978',
-        gw: ''
+        thumb: '0978'
     },
     989: {
         name: 'Fake SH',
-        thumb: '0989',
-        gw: ''
+        thumb: '0989'
     },
     1047: {
         name: 'Sabo',
-        thumb: '1047',
-        gw: ''
+        thumb: '1047'
     },
     1108: {
         name: 'Shiki',
-        thumb: '1108',
-        gw: ''
+        thumb: '1108'
     },
     1163: {
         name: 'Tesoro',
-        thumb: '1163',
-        gw: ''
+        thumb: '1163'
     },
     1298: {
         name: 'Akainu',
-        thumb: '1298',
-        gw: ''
+        thumb: '1298'
     },
     9999: {
         name: 'Mihawk *',
-        thumb: '9999',
-        gw: ''
+        thumb: '9999'
     },
 };
