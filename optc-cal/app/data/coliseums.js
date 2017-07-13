6a4df260b5b598102e8158f36e18f7a1396cc5bb
Zifeng Lu <lukforce@gmail.com>
2017-07-13 00:23:26 -0700

    Name for coliseums

--- optc-cal/app/data/coliseums.js
+++ optc-cal/app/data/coliseums.js
@@ -1,110 +1,146 @@
 var coliseums = {
     777: {
+        name: 'Lucky Roux',
         thumb: '0777'
     },
     779: {
+        name: 'Rebecca',
         thumb: '0779'
     },
     781: {
+        name: 'Urouge',
         thumb: '0781'
     },
     804: {
+        name: 'Hina',
         thumb: '0804'
     },
     806: {
+        name: 'Smoker',
         thumb: '0806'
     },
     831: {
+        name: 'Hawkins',
         thumb: '0831'
     },
     833: {
+        name: 'Wyper',
         thumb: '0833'
     },
     834: {
+        name: 'Mr. 7 & Ms. Father\'s Day',
         thumb: '0834'
     },
     860: {
+        name: 'Coby',
         thumb: '0860'
     },
     862: {
+        name: 'Alvida',
         thumb: '0862'
     },
     882: {
+        name: 'Apoo',
         thumb: '0882'
     },
     884: {
+        name: 'Dorry',
         thumb: '0884'
     },
     901: {
+        name: 'Franky',
         thumb: '0901'
     },
     903: {
+        name: 'Broggy',
         thumb: '0903'
     },
     904: {
+        name: 'Yokozuna',
         thumb: '0904'
     },
     924: {
+        name: 'Kid',
         thumb: '0924'
     },
     926: {
+        name: 'Sadie',
         thumb: '0926'
     },
     927: {
+        name: 'Doberman',
         thumb: '0927'
     },
     951: {
+        name: 'Ace',
         thumb: '0951'
     },
     952: {
+        name: 'Ben Beckman',
         thumb: '0952'
     },
     983: {
+        name: 'Vista',
         thumb: '0983'
     },
     984: {
+        name: 'Paulie',
         thumb: '0984'
     },
     1016: {
+        name: 'Capone Bege',
         thumb: '1016'
     },
     1018: {
+        name: 'Brownbeard',
         thumb: '1018'
     },
     1019: {
+        name: 'Lulu',
         thumb: '1019'
     },
     1041: {
+        name: 'Jozu',
         thumb: '1041'
     },
     1043: {
+        name: 'Killer',
         thumb: '1043'
     },
     1087: {
+        name: 'Musshuru',
         thumb: '1087'
     },
     1089: {
+        name: 'Saga',
         thumb: '1089'
     },
     1091: {
+        name: 'X-Drake',
         thumb: '1091'
     },
     1102: {
+        name: 'Zephyr',
         thumb: '1102'
     },
     1104: {
+        name: 'Ain',
         thumb: '1104'
     },
     1106: {
+        name: 'Binz',
         thumb: '1106'
     },
     1141: {
+        name: 'Moria',
         thumb: '1141'
     },
     1143: {
+        name: 'Foxy',
         thumb: '1143'
     },
     1145: {
+        name: 'Porche',
         thumb: '1145'
     },
 };
