945b5be9434f102dfaf87d7f3acf1bdb3a21c8d5
Zifeng Lu <lukforce@gmail.com>
2017-07-27 18:13:48 -0700

    Change Seven Days guides to YouTube

--- optc-cal/app/guides/sevenDays.js
+++ optc-cal/app/guides/sevenDays.js
@@ -1,137 +1,137 @@
 var sd = {
     // Fortnights
-    30: '',             // Alvida
-    39: '',             // Buggy
-    46: '',             // Kuro
-    58: '',             // Krieg
-    66: '',             // Arlong
-    228: '',            // Morgan
-    232: '',            // Hina
-    263: '',            // CobiMeppo
-    268: '',            // Smoker
-    297: '',            // Tsuru
-    318: '',            // Kalifa
-    322: '',            // Lucci
-    337: '',            // Franky
-    341: '',            // Kraken
-    386: '',            // Hogback
-    392: '',            // Perona
-    405: '',            // Absalom
-    420: '',            // Kimono Luffy
-    424: '',            // Brook
-    436: '',            // Candy Sanji
-    445: '',            // Karoo
-    462: '',            // Prisoner Mr. 2
-    510: '',            // Prisoner Croc
-    538: '',            // Domino
-    550: '',            // Sadie
-    571: '',            // Afro Luffy
-    583: '',            // Shandia
-    599: '',            // Pirate Apprentice
-    607: '',            // Zeff
-    635: '',            // Camie
-    656: '',            // Arlong Pirates
-    664: '',            // Sand Sand Band
-    690: '',            // Noland
-    713: '',            // Ganfor
-    724: '',            // T-Bone
-    743: '',            // Wanze
-    761: '',            // Halloween 1
-    766: '',            // Halloween 2
-    797: '',            // Breed
-    808: '',            // Wedding Nami
-    848: '',            // Tom's Workers
-    852: '',            // Wapol
-    918: '5g74ca',      // Kuja
-    929: '',            // Hannyabal
-    946: '',            // WB Pirates 1
-    979: '5pgvj6',      // WB Pirates 2
-    1003: '5r8x0o',     // Heart Pirates
-    1011: '',           // Count Butler
-    1037: '5u1aqj',     // Ivankov
-    1049: '',           // Nico Olvia
-    1072: '56stdk',     // Dead End
-    1095: '58z50x',     // Golden Lion Pirates
-    1112: '5bnqyb',     // Young Nami Robin
-    1137: '',           // CP9
-    1167: '5ds8zn',     // Baccarat
-    1203: '',           // Yorki
-    1215: '64qv0r',     // World Pirates
-    1242: '67jrgp',     // Revolutionary Army
-    1254: '',           // WB Pirates 3
-    1287: '6c5nym',     // Donquixote Children
-    1302: '',           // Prisoner Buggy
-    1328: '6idwkh',     // Tontatta 1
-    1332: '6l9tc9',     // Tontatta 2
-    1357: '',           // Big Mom Pirates
-    1384: '',           // Toy Soldier
+    30: '',                 // Alvida
+    39: '',                 // Buggy
+    46: '',                 // Kuro
+    58: '',                 // Krieg
+    66: '',                 // Arlong
+    228: '',                // Morgan
+    232: '',                // Hina
+    263: '',                // CobiMeppo
+    268: '',                // Smoker
+    297: '',                // Tsuru
+    318: '',                // Kalifa
+    322: '',                // Lucci
+    337: '',                // Franky
+    341: '',                // Kraken
+    386: '',                // Hogback
+    392: '',                // Perona
+    405: '',                // Absalom
+    420: '',                // Kimono Luffy
+    424: '',                // Brook
+    436: '',                // Candy Sanji
+    445: '',                // Karoo
+    462: '',                // Prisoner Mr. 2
+    510: '',                // Prisoner Croc
+    538: '',                // Domino
+    550: '',                // Sadie
+    571: '',                // Afro Luffy
+    583: '',                // Shandia
+    599: '',                // Pirate Apprentice
+    607: '',                // Zeff
+    635: '',                // Camie
+    656: '',                // Arlong Pirates
+    664: '',                // Sand Sand Band
+    690: '',                // Noland
+    713: '',                // Ganfor
+    724: '',                // T-Bone
+    743: '',                // Wanze
+    761: '',                // Halloween 1
+    766: '',                // Halloween 2
+    797: 'SF5wvpx2y_M',     // Breed
+    808: 'fDzd1CvFi70',     // Wedding Nami
+    848: 'j226tz_oRh8',     // Tom's Workers
+    852: 'i4Wlw8gk1mU',     // Wapol
+    918: 'J-43fLX58nk',     // Kuja
+    929: 'FIGMaJ8mqDo',     // Hannyabal
+    946: 'qISwpYVldC0',     // WB Pirates 1
+    979: '-yuVqxxLC6E',     // WB Pirates 2
+    1003: 'AqVaX5zvjIo',    // Heart Pirates
+    1011: 'Z_-wBOdUCfs',    // Count Butler
+    1037: 'U9Jx5OwPBKs',    // Ivankov
+    1049: '3QZMBFyjLvI',    // Nico Olvia
+    1072: 'xQpUoSyH428',    // Dead End
+    1095: 'pyJnbfwaKbY',    // Golden Lion Pirates
+    1112: '1od7LciC4UE',    // Young Nami Robin
+    1137: 'yNdyWMYbrOc',    // CP9
+    1167: 'gISYleUoc9E',    // Baccarat
+    1203: '',               // Yorki
+    1215: 'uAU9P4lv71k',    // World Pirates
+    1242: '2HSGlW_N3hY',    // Revolutionary Army
+    1254: '1dwQtF9Rdpc',    // WB Pirates 3
+    1287: 'vn_ZATg5gfU',    // Donquixote Children
+    1302: 'sNDdKUpAjyk',    // Prisoner Buggy
+    1328: 'a1E50n6Hr2w',    // Tontatta 1
+    1332: 'tNWn2PPQHuM',    // Tontatta 2
+    1357: 'PwWC9qDbNJY',    // Big Mom Pirates
+    1384: '',               // Toy Soldier
 
     // Raids
-    227: '',            // Mihawk
-    249: '',            // Monster Chopper
-    306: '',            // Garp
-    353: '',            // Zephyr
-    390: '4bbwxf',      // Ivankov
-    418: '',            // Doffy
-    447: '',            // Blackbeard
-    516: '',            // Heracles-un
-    518: '',            // Usopp-un
-    575: '',            // Aokiji
-    603: '4bgrqd',      // Eneru
-    654: '',            // Duval
-    771: '',            // Buster Call
-    795: '6g3heb',      // Nightmare Luffy
-    836: '',            // Kuma
-    865: '',            // Boa Hancock
-    933: '',            // Pica
-    978: '5q46wh',      // Kizaru
-    989: '62yr0m',      // Fake Straw Hats
-    1047: '5yilwa',     // Sabo
-    1108: '',           // Shiki
-    1163: '',           // Gild Tesoro
-    1298: '',           // Akainu
+    227: '',                // Mihawk
+    249: '',                // Monster Chopper
+    306: '',                // Garp
+    353: 'yny9vPWAVEM',     // Zephyr
+    390: '',                // Ivankov
+    418: '',                // Doffy
+    447: '',                // Blackbeard
+    516: '',                // Heracles-un
+    518: '',                // Usopp-un
+    575: '',                // Aokiji
+    603: '',                // Eneru
+    654: '',                // Duval
+    771: '58nDLiPI3JQ',     // Buster Call
+    795: 'jGBG_Js_WJI',     // Nightmare Luffy
+    836: 'OdQD77HK_uQ',     // Kuma
+    865: 'OlQFG9OgXg8',     // Boa Hancock
+    933: 'e19nPcmNDBI',     // Pica
+    978: '8H9z_0jnr0A',     // Kizaru
+    989: 'js_iwp0KgeI',     // Fake Straw Hats
+    1047: 'RToTWoDEdWc',    // Sabo
+    1108: 'JuVUShOH-08',    // Shiki
+    1163: 'q1wNq-RIrR0',    // Gild Tesoro
+    1298: 'r5eGwr0YXLQ',    // Akainu
 
-    1623: '',           // Doflamingo (N)
-    1680: '',           // Mihawk (N)
+    1623: '',               // Doflamingo (N)
+    1680: '-9bIzVawWdg',    // Mihawk (N)
 
-    1258: '',           // Young Whitebeard
-    1380: '',           // Shanks
+    1258: '',               // Young Whitebeard
+    1380: '',               // Shanks
 
     // Coliseums
-    777: '52ka1o',      // Lucky Roux
-    779: '52ka1o',      // Rebecca
-    781: '52ka1o',      // Urouge
-    804: '55olrb',      // Hina
-    806: '55olrb',      // Smoker
-    831: '',            // Hawkins
-    833: '5or2kh',      // Wyper
-    834: '',            // Mr. 7 & Ms. Father's Day
-    860: '5or2kh',      // Coby
-    862: '5or2kh',      // Alvida
-    882: '5ktdxf',      // Apoo
-    884: '5ktdxf',      // Dorry
-    901: '5swjdr',      // Franky
-    903: '5swjdr',      // Broggy
-    904: '5swjdr',      // Yokozuna
-    924: '5db245',      // Kid
-    926: '5db245',      // Sadie
-    927: '5db245',      // Doberman
-    951: '',            // Ace
-    952: '',            // Ben Beckman
-    983: '5x06mc',      // Vista
-    984: '5x06mc',      // Paulie
-    1016: '60wsvz',     // Capone Bege
-    1018: '651uaf',     // Brownbeard
-    1019: '60wsvz',     // Lulu
-    1041: '651uaf',     // Jozu
-    1043: '693nmd',     // Killer
-    1087: '6lhkbs',     // Musshuru
-    1089: '6lhkbs',     // Saga
-    1091: '693nmd',     // X-Drake
-    1102: '6h92m6',     // Zephyr
-    1104: '6h92m6',     // Ain
-    1106: '6h92m6',     // Binz
-    1141: '',           // Moria
-    1143: '',           // Foxy
-    1145: '',           // Porche
+    777: '',                // Lucky Roux
+    779: '',                // Rebecca
+    781: '7vvJvYlihJo',     // Urouge
+    804: '9Kv8pqGxsPA',     // Hina
+    806: '61-lcVVohHI',     // Smoker
+    831: 'XDXhnr2DL84',     // Hawkins
+    833: 'W8lFvVOpWgI',     // Wyper
+    834: '',                // Mr. 7 & Ms. Father's Day
+    860: 'TcNI42y4d4M',     // Coby
+    862: 'vLt7GNQ1bjE',     // Alvida
+    882: 'OIpYCcatYj8',     // Apoo
+    884: 'Cf6sxR86u1Y',     // Dorry
+    901: 'wxStmpK6zrY',     // Franky
+    903: 'CAgpUWVSCFk',     // Broggy
+    904: '',                // Yokozuna
+    924: 'ZS3mUSzdXvE',     // Kid
+    926: '_vW9hYAuM5I',     // Sadie
+    927: '-7WxyLzL9EA',     // Doberman
+    951: '2XhDa1t7gZU',     // Ace
+    952: 'eNCUYorlSME',     // Ben Beckman
+    983: 'V-Fu4KP6vvo',     // Vista
+    984: '',                // Paulie
+    1016: '0WyCclRbwqE',    // Capone Bege
+    1018: 'N9L-o58rKrM',    // Brownbeard
+    1019: '',               // Lulu
+    1041: 'UP3s9ExtH8I',    // Jozu
+    1043: 'WwBv2HVEmcM',    // Killer
+    1087: '3U5H9DSwkYE',    // Musshuru
+    1089: '2h3gzslNBsk',    // Saga
+    1091: 'Xoc3dCKWNqM',    // X-Drake
+    1102: 'gQrisKrX1w4',    // Zephyr
+    1104: 'a0niFXU2nJ8',    // Ain
+    1106: 'p2sug3M7BCk',    // Binz
+    1141: 'Qrg0iUyS2zs',    // Moria
+    1143: '',               // Foxy
+    1145: '',               // Porche
 };
