276ac6237cd47d0b5462019eac464544f8ee8b9f
Zifeng Lu <lukforce@gmail.com>
2017-08-08 20:18:01 -0700

    Minor semi colon fix

--- optc-cal/app/guides/gw.js
+++ optc-cal/app/guides/gw.js
@@ -91,7 +91,7 @@ var gw = {
     1163: '35950',      // Gild Tesoro
     1298: '41277',      // Akainu
 
-    1623: '';           //'58437',     // Doflamingo (N)
+    1623: '',           //'58437',     // Doflamingo (N)
     1680: '62444',      // Mihawk (N)
 
     1258: '38237',      // Young Whitebeard
