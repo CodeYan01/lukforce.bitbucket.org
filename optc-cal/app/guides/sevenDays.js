0ca488da5c5d0f8cfa6bb199d9a3f06cf0786f05
Zifeng Lu <lukforce@gmail.com>
2018-01-26 16:51:29 -0800

    Seven Days guide update

--- optc-cal/app/guides/sevenDays.js
+++ optc-cal/app/guides/sevenDays.js
@@ -90,7 +90,7 @@ var sd = {
     1606: '',               // BB Pirates
     1624: 'uv_a-F20vI0',    // New Fishman
     1649: 'BcSqVEGd2BE',    // Giolla & Brook
-    1670: '',               // Madam Sharley
+    1670: 'ww5CS9-aNQE',    // Madam Sharley
 
     // Raids
     227: '',                // Mihawk
