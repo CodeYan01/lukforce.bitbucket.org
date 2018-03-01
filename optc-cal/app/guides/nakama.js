71dc3a898425842322ff057d525bd89b5bfb422c
Zifeng Lu <lukforce@gmail.com>
2018-02-28 18:44:51 -0800

    [Calendar] Nakama Network link reassigned

--- optc-cal/app/guides/nakama.js
+++ optc-cal/app/guides/nakama.js
@@ -1,208 +1,208 @@
 var nakama = {
     // Fortnights
-    30: '2048070',      // Alvida
-    39: '2067982',      // Buggy
-    46: '2054137',      // Kuro
-    58: '2046884',      // Krieg
-    66: '2063774',      // Arlong
-    228: '2044406',     // Morgan
-    232: '2063744',     // Hina
-    263: '2082189',     // CobiMeppo
-    268: '2068183',     // Smoker
-    297: '2069274',     // Tsuru
-    318: '2079262',     // Kalifa
-    322: '2067104',     // Lucci
-    337: '2083902',     // Franky
-    341: '2044215',     // Kraken
-    386: '2078259',     // Hogback
-    392: '2066356',     // Perona
-    405: '2100886',     // Absalom
-    420: '2082812',     // Kimono Luffy
-    424: '2064839',     // Brook
-    436: '2071630',     // Candy Sanji
-    445: '2077303',     // Karoo
-    462: '2081444',     // Prisoner Mr. 2
-    510: '2084431',     // Prisoner Croc
-    538: '2077100',     // Domino
-    550: '2053391',     // Sadie
-    571: '2072678',     // Afro Luffy
-    583: '2085230',     // Shandia
-    599: '2087966',     // Pirate Apprentice
-    607: '2070422',     // Zeff
-    635: '2094090',     // Camie
-    656: '2083651',     // Arlong Pirates
-    664: '2061551',     // Sand Sand Band
-    681: '6073100',     // Summer Nami
-    683: '6073100',     // Summer Robin
-    685: '6073100',     // Summer Perona
-    690: '2046015',     // Noland
-    713: '2049088',     // Ganfor
-    724: '2074962',     // T-Bone
-    743: '2063618',     // Wanze
-    761: '2087688',     // Halloween 1
-    766: '2086896',     // Halloween 2
-    797: '2073876',     // Breed
-    808: '2061380',     // Wedding Nami
-    848: '2040390',     // Tom's Workers
-    852: '2045929',     // Wapol
-    866: '2142655',     // Kimono Robin
-    885: '2117830',     // Kimono Jinbe
-    897: '2119918',     // Kimono Croc
-    918: '2106825',     // Kuja
-    929: '2078637',     // Hannyabal
-    946: '2089510',     // WB Pirates 1
-    979: '2075362',     // WB Pirates 2
-    1003: '2108099',    // Heart Pirates
-    1011: '2096300',    // Count Butler
-    1037: '2093357',    // Ivankov
-    1049: '2074549',    // Nico Olvia
-    1072: '2083542',    // Dead End
-    1095: '2081225',    // Golden Lion Pirates
-    1112: '2146175',    // Young Nami Robin
-    1137: '2123359',    // CP9
-    1167: '2078991',    // Baccarat
-    1199: '6073100',    // Summer Bonney
-    1201: '6073100',    // Summer Violet
-    1203: '2045646',    // Rumbar Pirates
-    1215: '2042682',    // World Pirates
-    1242: '2060194',    // Revolutionary Army
-    1254: '2080840',    // WB Alliances
-    1287: '2130129',    // Donquixote Kids
-    1300: '6081218',    // Halloween Rebecca
-    1302: '2090756',    // Prisoner Buggy
-    1306: '6081218',    // Halloween Sugar
-    1326: '6081218',    // Halloween Corazon
-    1328: '2107554',    // Tontatta 1
-    1332: '2119118',    // Tontatta 2
-    1357: '2048913',    // Big Mom Pirates
-    1384: '2075017',    // Toy Soldier
-    1397: '2088050',    // Shambles G5
-    1416: '2101780',    // Sengoku
-    1426: '2044619',    // Don Sai
-    1450: '2086259',    // Violet
-    1469: '2081422',    // Sentomaru
-    1509: '2089287',    // WB Pirates 3
-    1520: '2062448',    // Dadan
-    1547: '2056362',    // Soul King
-    1564: '2068018',    // Elizabello
-    1581: '2071459',    // Law
-    1597: '2079012',    // Shirahoshi
-    1606: '2114726',    // BB Pirates
-    1624: '2142476',    // New Fishman
-    1649: '2110068',    // Giolla & Brook
-    1670: '2038800',    // Madam Sharley
-    1690: '2035243',    // Kid
+    30: '2003000',      // Alvida
+    39: '2003900',      // Buggy
+    46: '2004600',      // Kuro
+    58: '2005800',      // Krieg
+    66: '2006600',      // Arlong
+    228: '2022800',     // Morgan
+    232: '2023200',     // Hina
+    263: '2026300',     // CobiMeppo
+    268: '2026800',     // Smoker
+    297: '2029700',     // Tsuru
+    318: '2031800',     // Kalifa
+    322: '2032200',     // Lucci
+    337: '2033700',     // Franky
+    341: '2034100',     // Kraken
+    386: '2038600',     // Hogback
+    392: '2039200',     // Perona
+    405: '2040500',     // Absalom
+    420: '2042000',     // Kimono Luffy
+    424: '2042400',     // Brook
+    436: '2043600',     // Candy Sanji
+    445: '2044500',     // Karoo
+    462: '2046200',     // Prisoner Mr. 2
+    510: '2051000',     // Prisoner Croc
+    538: '2053800',     // Domino
+    550: '2055000',     // Sadie
+    571: '2057100',     // Afro Luffy
+    583: '2058300',     // Shandia
+    599: '2059900',     // Pirate Apprentice
+    607: '2060700',     // Zeff
+    635: '2063500',     // Camie
+    656: '2065600',     // Arlong Pirates
+    664: '2066400',     // Sand Sand Band
+    681: '6068300',     // Summer Nami
+    683: '6068300',     // Summer Robin
+    685: '6068300',     // Summer Perona
+    690: '2068800',     // Noland
+    713: '2071300',     // Ganfor
+    724: '2072400',     // T-Bone
+    743: '2074300',     // Wanze
+    761: '2076100',     // Halloween 1
+    766: '2076600',     // Halloween 2
+    797: '2079700',     // Breed
+    808: '2080800',     // Wedding Nami
+    848: '2084800',     // Tom's Workers
+    852: '2085200',     // Wapol
+    866: '2086600',     // Kimono Robin
+    885: '2088500',     // Kimono Jinbe
+    897: '2089700',     // Kimono Croc
+    918: '2091800',     // Kuja
+    929: '2092900',     // Hannyabal
+    946: '2094600',     // WB Pirates 1
+    979: '2097900',     // WB Pirates 2
+    1003: '2100300',    // Heart Pirates
+    1011: '2101100',    // Count Butler
+    1037: '2103700',    // Ivankov
+    1049: '2104900',    // Nico Olvia
+    1072: '2107200',    // Dead End
+    1095: '2109500',    // Golden Lion Pirates
+    1112: '2111200',    // Young Nami Robin
+    1137: '2113700',    // CP9
+    1167: '2116700',    // Baccarat
+    1199: '6068300',    // Summer Bonney
+    1201: '6068300',    // Summer Violet
+    1203: '2120300',    // Rumbar Pirates
+    1215: '2121500',    // World Pirates
+    1242: '2124200',    // Revolutionary Army
+    1254: '2125400',    // WB Alliances
+    1287: '2128700',    // Donquixote Kids
+    1300: '6130000',    // Halloween Rebecca
+    1302: '2130400',    // Prisoner Buggy
+    1306: '6130000',    // Halloween Sugar
+    1326: '6130000',    // Halloween Corazon
+    1328: '2132800',    // Tontatta 1
+    1332: '2133200',    // Tontatta 2
+    1357: '2135700',    // Big Mom Pirates
+    1384: '2138400',    // Toy Soldier
+    1397: '2139700',    // Shambles G5
+    1416: '2141600',    // Sengoku
+    1426: '2142600',    // Don Sai
+    1450: '2145000',    // Violet
+    1469: '2146900',    // Sentomaru
+    1509: '2150900',    // WB Pirates 3
+    1520: '2152000',    // Dadan
+    1547: '2154700',    // Soul King
+    1564: '2156400',    // Elizabello
+    1581: '2158100',    // Law
+    1597: '2159700',    // Shirahoshi
+    1606: '2160600',    // BB Pirates
+    1624: '2162400',    // New Fishman
+    1649: '2164900',    // Giolla & Brook
+    1670: '2167000',    // Madam Sharley
+    1690: '2169000',    // Kid
     1725: '2172300',    // Neptune Family
 
     // Raids
-    227: '4039081',     // Mihawk
-    249: '4070990',     // Monster Chopper
-    306: '4070492',     // Garp
-    353: '4067724',     // Zephyr
-    390: '4042949',     // Ivankov
-    418: '4051900',     // Doffy
-    447: '4050751',     // Blackbeard
-    516: '4049780',     // Heracles-un
-    518: '4042359',     // Usopp-un
-    575: '4038759',     // Aokiji
-    603: '4036043',     // Eneru
-    654: '4035948',     // Duval
-    771: '4053420',     // Buster Call
-    795: '4066936',     // Nightmare Luffy
-    836: '4054403',     // Kuma
-    865: '4087409',     // Boa Hancock
-    933: '4032020',     // Pica
-    978: '4039716',     // Kizaru
-    989: '4101603',     // Fake Straw Hats
-    1047: '4032256',    // Sabo
-    1108: '4035822',    // Shiki
-    1163: '4053803',    // Gild Tesoro
-    1298: '4038813',    // Akainu
-    1374: '4065831',    // Magellan
-    1518: '4042191',    // Bellamy
-    1595: '4046083',    // Fujitora
-    1696: '4036175',    // Vergo
-    1815: '4063867',    // Sanji
-    1924: '4045622',    // Rayleigh
+    227: '4022700',     // Mihawk
+    249: '4024900',     // Monster Chopper
+    306: '4030600',     // Garp
+    353: '4035300',     // Zephyr
+    390: '4039000',     // Ivankov
+    418: '4041800',     // Doffy
+    447: '4044700',     // Blackbeard
+    516: '4051600',     // Heracles-un
+    518: '4051800',     // Usopp-un
+    575: '4057500',     // Aokiji
+    603: '4060300',     // Eneru
+    654: '4065400',     // Duval
+    771: '4077100',     // Buster Call
+    795: '4079500',     // Nightmare Luffy
+    836: '4083600',     // Kuma
+    865: '4086500',     // Boa Hancock
+    933: '4093200',     // Pica
+    978: '4097800',     // Kizaru
+    989: '4098900',     // Fake Straw Hats
+    1047: '4104700',    // Sabo
+    1108: '4110800',    // Shiki
+    1163: '4116300',    // Gild Tesoro
+    1298: '4129800',    // Akainu
+    1374: '4137400',    // Magellan
+    1518: '4151800',    // Bellamy
+    1595: '4159500',    // Fujitora
+    1696: '4169600',    // Vergo
+    1815: '4181500',    // Sanji
+    1924: '4192400',    // Rayleigh
     5012: '4202500',    // Doffy v2
 
-    1623: '4062886',    // Doflamingo (N)
-    1669: '4049729',    // Aokiji (N)
-    1680: '4050051',    // Mihawk (N)
-    1727: '4043234',    // Sabo (N)
-    1739: '4053201',    // Bellamy (N)
-    1780: '4050686',    // Kizaru (N)
+    1623: '4162300',    // Doflamingo (N)
+    1669: '4166900',    // Aokiji (N)
+    1680: '4168000',    // Mihawk (N)
+    1727: '4172700',    // Sabo (N)
+    1739: '4173800',    // Bellamy (N)
+    1780: '4178000',    // Kizaru (N)
 
-    1258: '6080102',    // Young Whitebeard
-    1380: '6073782',    // Shanks
-    1530: '6058401',    // Cavendish
+    1258: '6125800',    // Young Whitebeard
+    1380: '6138000',    // Shanks
+    1530: '6153000',    // Cavendish
 
     // Coliseums
-    777: '5097899',     // Lucky Roux
-    779: '5158488',     // Rebecca
-    781: '5174150',     // Urouge
-    804: '5129770',     // Hina
-    806: '5137084',     // Smoker
-    831: '5183584',     // Hawkins
-    833: '5139953',     // Wyper
-    834: '5136256',     // Mr. 7 & Ms. Father's Day
-    860: '5148616',     // Coby
-    862: '5146907',     // Alvida
-    882: '5203477',     // Apoo
-    884: '5154857',     // Dorry
-    901: '5164452',     // Franky
-    903: '5157518',     // Broggy
-    904: '5117767',     // Yokozuna
-    924: '5153654',     // Kid
-    926: '5148659',     // Sadie
-    927: '5177338',     // Doberman
-    951: '5165214',     // Ace
-    953: '5122809',     // Ben Beckman
-    983: '5172144',     // Vista
-    984: '5190671',     // Paulie
-    1016: '5197735',    // Capone Bege
-    1018: '5102205',    // Brownbeard
-    1019: '5228376',    // Lulu
-    1041: '5168956',    // Jozu
-    1043: '5125703',    // Killer
-    1087: '5097620',    // Musshuru
-    1089: '5082110',    // Saga
-    1091: '5175199',    // X-Drake
-    1102: '5117789',    // Zephyr
-    1104: '5115395',    // Ain
-    1106: '5119299',    // Binz
-    1141: '5177549',    // Moria
-    1196: '5154202',    // Ganfor
-    1220: '5110825',    // Byrnndi World
-    1251: '5144984',    // Marco
-    1310: '5092591',    // Kinemon
-    1338: '5092920',    // Kanjuro
-    1378: '5159871',    // Marguerite
-    1401: '5127538',    // Gladius
-    1422: '5149842',    // Diamante
-    1423: '5135216',    // T-Bone
-    1456: '5136553',    // Don Chin Jao
-    1458: '5130466',    // Kuro
-    1460: '5167718',    // Ms. Valentine
-    1487: '5127799',    // Ideo
-    1525: '5237291',    // Suleiman
-    1553: '5189523',    // Hajrudin
-    1584: '5191393',    // Orlumbus
-    1602: '5148472',    // Shiryu
-    1603: '5125281',    // McGuy
-    1628: '5153634',    // Machvise
-    1667: '5151816',    // Kyros
-    1694: '5145772',    // Inuarashi
-    1733: '5191415',    // Nekomamushi
-    1792: '5231789',    // Lucy
-    1828: '5141993',    // Shishilian
+    777: '5077601',     // Lucky Roux
+    779: '5077801',     // Rebecca
+    781: '5078001',     // Urouge
+    804: '5080301',     // Hina
+    806: '5080501',     // Smoker
+    831: '5083001',     // Hawkins
+    833: '5083201',     // Wyper
+    834: '5083401',     // Mr. 7 & Ms. Father's Day
+    860: '5085901',     // Coby
+    862: '5086101',     // Alvida
+    882: '5088101',     // Apoo
+    884: '5088301',     // Dorry
+    901: '5090001',     // Franky
+    903: '5090201',     // Broggy
+    904: '5090401',     // Yokozuna
+    924: '5092301',     // Kid
+    926: '5092501',     // Sadie
+    927: '5092701',     // Doberman
+    951: '5095001',     // Ace
+    953: '5095201',     // Ben Beckman
+    983: '5098201',     // Vista
+    984: '5098401',     // Paulie
+    1016: '5101501',    // Capone Bege
+    1018: '5101701',    // Brownbeard
+    1019: '5101901',    // Lulu
+    1041: '5104001',    // Jozu
+    1043: '5104201',    // Killer
+    1087: '5108601',    // Musshuru
+    1089: '5108801',    // Saga
+    1091: '5109001',    // X-Drake
+    1102: '5110101',    // Zephyr
+    1104: '5110301',    // Ain
+    1106: '5110501',    // Binz
+    1141: '5114001',    // Moria
+    1196: '5119501',    // Ganfor
+    1220: '5121901',    // Byrnndi World
+    1251: '5125001',    // Marco
+    1310: '5130901',    // Kinemon
+    1338: '5133701',    // Kanjuro
+    1378: '5137701',    // Marguerite
+    1401: '5140001',    // Gladius
+    1422: '5142101',    // Diamante
+    1423: '5142301',    // T-Bone
+    1456: '5145501',    // Don Chin Jao
+    1458: '5145701',    // Kuro
+    1460: '5145901',    // Ms. Valentine
+    1487: '5148601',    // Ideo
+    1525: '5152501',    // Suleiman
+    1553: '5155201',    // Hajrudin
+    1584: '5158301',    // Orlumbus
+    1602: '5160101',    // Shiryu
+    1603: '5160301',    // McGuy
+    1628: '5162701',    // Machvise
+    1667: '5166601',    // Kyros
+    1694: '5169301',    // Inuarashi
+    1733: '5173201',    // Nekomamushi
+    1792: '5179101',    // Lucy
+    1828: '5182701',    // Shishilian
 
     // Event
-    'bb_1314': '6094406',   // Three Admirals
-    'bb_1404': '6092400',   // Straw Hat Pirates
-    'bb_0870': '6093262',   // Donquixote Family
-    '20_1684': '6078907',   // 20th Anni SH Showdown 1
-    '20_1682': '6079819',   // 20th Anni SH Mission 1
-    '20_1755': '6064534',   // 20th Anni SH Showdown 2
-    '20_1753': '6159457',   // 20th Anni SH Mission 2
+    'bb_1314': '6131400',   // Three Admirals
+    'bb_1404': '6140400',   // Straw Hat Pirates
+    'bb_0870': '6087000',   // Donquixote Family
+    '20_1684': '6168400',   // 20th Anni SH Showdown 1
+    '20_1682': '6168600',   // 20th Anni SH Mission 1
+    '20_1755': '6175500',   // 20th Anni SH Showdown 2
+    '20_1753': '6175700',   // 20th Anni SH Mission 2
 };
