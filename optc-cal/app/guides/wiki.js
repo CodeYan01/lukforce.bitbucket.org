a2effc58487a7a38a153cc6e10035a65b3710e62
Zifeng Lu <lukforce@gmail.com>
2018-04-23 00:28:18 -0700

    [Calendar] Wiki guide update

--- optc-cal/app/guides/wiki.js
+++ optc-cal/app/guides/wiki.js
@@ -96,7 +96,7 @@ var wiki = {
     1735: 'comments/84d7up',                // Sugar & Trebol
     1766: 'comments/87p15b',                // Blue Gilly
     1774: 'comments/89m7ks',                // Kinemon
-    1810: '',                               // Wanda & Carrot
+    1810: 'comments/8drnkw',                // Wanda & Carrot
 
     // Raids
     227: 'wiki/raidboss/mihawk',
