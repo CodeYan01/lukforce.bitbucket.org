2c9be8b75c227ce5b639a45ba7e99c80d0ca94a0
Zifeng Lu <lukforce@gmail.com>
2020-03-17 14:25:42 -0700

    [Calendar] Drop link update

--- optc-cal/app/guides/drops.js
+++ optc-cal/app/guides/drops.js
@@ -144,8 +144,8 @@ drops = {
     2633: 'Pirate%20Expo!%20Festival%20Fortnight',
     2635: 'Pirate%20Expo!%20Vacation%20Fortnight',
     2665: 'Diva%20Ann%27s%20Special%20Live%20Show%20Fortnight',
-    2692: 'Impel%20Down%20Breakout%20Fortnight',
-    2723: 'Thinking%20about%20Pops%20Fortnight',
+    2692: 'Escapees,%20Join%20the%20Fight!%20Fortnight',
+    2723: 'Remembering%20Pops%20Fortnight',
 
     // 20 Stamina Raids
     518: 'Clash!?%20Usopp%27n%20Raid',
