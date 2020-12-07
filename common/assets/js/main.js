0ac1a832bf3d54a3a766379c1801bb3c9ead9d53
Zifeng Lu <lukforce@gmail.com>
2020-12-06 23:11:15 -0800

    [General] Add thumbs for new units not on official site

--- common/assets/js/main.js
+++ common/assets/js/main.js
@@ -94,13 +94,8 @@ function getThumb(thumbId) {
         return '/tm-planner/assets/img/new-thumbs/5068.png';
 
     // Special case for thumbs not on official website
-    /*if (
-        thumbId === 3080 ||
-        thumbId === 3081 ||
-        thumbId > 3083 && thumbId < 3333
-    )
-        return 'https://onepiecetreasurecruise.fr/img/OPTCDB/uploads/f' + paddedThumbId + '.png';
-    */
+    if (thumbId > 3086 && thumbId < 3333)
+        return '/tm-planner/assets/img/new-thumbs/f' + paddedThumbId + '.png';
 
     // Special cases for new thumbs
     if (
