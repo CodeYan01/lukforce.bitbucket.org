c77f57db805a172db92484b8aca478a99cbf9e04
Zifeng Lu <lukforce@gmail.com>
2022-05-15 21:16:29 -0700

    [TM Planner] Synced Special, Sailor, Captain filters from the DB update

--- tm-planner/data/filter_map_ca.js
+++ tm-planner/data/filter_map_ca.js
@@ -1,8 +1,8 @@
 var filter_map_ca = {
-    'slot-pos':                 /orbs beneficial/i,
-    'bind-red':                 /(reduces|removes).+bind.+duration/i,
-    'desp-red':                 /(reduces|removes).+despair.+duration/i,
+    'slot-pos':                 /makes ([^".]+?)orbs beneficial for ([^".]+?)characters/i,
+    'bind-red':                 /(?:reduces|removes)(?: |[^."]+?, |[^."]+? and )bind[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))( (?:for|on) \w+ characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
+    'desp-red':                 /(?:reduces|removes)(?: |[^."]+? and |[^."]+?, )despair[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
     'cd-red':                   /restores.+special cooldowns?.+rewinded/i,
-    'silence-red':              /(reduces|removes).+silence.+duration/i,
-    'para-red':                 /(reduces|removes).+paralysis.+duration/i
+    'silence-red':              /(?:reduces|removes)[^."]+?silence[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
+    'para-red':                 /(?:reduces|removes)[^."]+?paralysis[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i
 };
--- tm-planner/data/filter_map_sl.js
+++ tm-planner/data/filter_map_sl.js
@@ -1,7 +1,7 @@
 var filter_map_sl = {
-    'slot-pos':                 /beneficial/i,
-    'cd-red':                   /Restores own special/i,
-    'silence-red':              /(reduces|removes|resists).+silence/i,
-    'para-red':                 /(reduces|removes|resists).+Paralysis/i,
-    'blind-red':                /(reduces|removes|resists).+blindness/i
+    'slot-pos':                 /makes ([^".]+?)orbs beneficial for ([^".]+?)characters/i,
+    'cd-red':                   /restores.+special cooldowns?.+rewinded/i,
+    'silence-red':              /(?:reduces|removes)[^."]+?silence[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
+    'para-red':                 /(?:reduces|removes)[^."]+?paralysis[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?: on ([^."]+?)characters?)?(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i,
+    'blind-red':                /(?:reduces|removes)[^."]+?(?:blindness|selected debuffs?)[^."]+?duration (?:by ([?\d]+)(?:-([?\d]+))? turns?|(completely))(?:, by ([?\d]+)(?:-([?\d]+))? turns?)?/i
 };
